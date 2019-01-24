a34f0252a2663b131aca316531dcf3fc012a7c6c
SeongJun Ha <agopwns@naver.com>
2019-01-24 21:34:13 +0900

    2019-01-24 Youtube Clone Coding #4
    2019-01-24 Youtube Clone Coding #4

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:32493,
+        title:'Video awesome',
+        description: 'This is somthing I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators :{
+            id:121211,
+            name:"SeongJun",
+            email:"agopwns@naver.com"
+        }
+    },
+    {
+        id:12345,
+        title:'Video awesome1',
+        description: 'This is somthing I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators :{
+            id:121211,
+            name:"SeongJun",
+            email:"agopwns@naver.com"
+        }
+    },
+    {
+        id:25555,
+        title:'Video awesome2',
+        description: 'This is somthing I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators :{
+            id:121211,
+            name:"SeongJun",
+            email:"agopwns@naver.com"
+        }
+    },
+    {
+        id:12311,
+        title:'Video awesome3',
+        description: 'This is somthing I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators :{
+            id:121211,
+            name:"SeongJun",
+            email:"agopwns@naver.com"
+        }
+    }
+];
