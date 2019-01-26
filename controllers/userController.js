094825f2e747df7740dcbb5117374484d3327769
SeongJun Ha <agopwns@naver.com>
2019-01-26 19:49:02 +0900

    2019-01-26 Youtube Clone Coding #5
    2019-01-26 Youtube Clone Coding #5

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,15 +16,21 @@ export const postJoin = (req, res) => {
         // ToDo : Log User in
         res.redirect(routes.home);
     }
-}
+};
 
-
-
-
-export const login = (req, res) => 
+export const getLogin = (req, res) => 
     res.render("login", { pageTitle : "Login" });
-export const logout = (req, res) => 
-    res.render("logout", { pageTitle : "Logout" });
+
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
+
+export const logout = (req, res) => {
+    // ToDo: Porcess Log Out
+    res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) => 
     res.render("userDetail", { pageTitle : "User Detail" });
 export const editProfile = (req, res) => 
