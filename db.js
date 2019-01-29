d2f9fa7188809c6541853d8d1aad030cce9c4189
SeongJun Ha <agopwns@naver.com>
2019-01-29 22:31:50 +0900

    Youtube Clone Coding #11
    Connect to MongoDB and Create Schema

--- db.js
+++ db.js
@@ -1,50 +1,19 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id:32493,
-        title:'Video awesome',
-        description: 'This is somthing I love',
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creators :{
-            id:121211,
-            name:"SeongJun",
-            email:"agopwns@naver.com"
-        }
-    },
-    {
-        id:12345,
-        title:'Video awesome1',
-        description: 'This is somthing I love',
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creators :{
-            id:121211,
-            name:"SeongJun",
-            email:"agopwns@naver.com"
-        }
-    },
-    {
-        id:25555,
-        title:'Video awesome2',
-        description: 'This is somthing I love',
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creators :{
-            id:121211,
-            name:"SeongJun",
-            email:"agopwns@naver.com"
-        }
-    },
-    {
-        id:12311,
-        title:'Video awesome3',
-        description: 'This is somthing I love',
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creators :{
-            id:121211,
-            name:"SeongJun",
-            email:"agopwns@naver.com"
-        }
+        userNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOPen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌  Error on DB Connection:${error}`);
+
+db.once("open", handleOPen);
+db.on("error", handleError);
