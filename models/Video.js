e57c4e759ca077c68177518984fe5b3398dabab0
SeongJun Ha <agopwns@naver.com>
2019-02-02 20:20:56 +0900

    # Youtube Clone Coding # 13
    #3.8 Getting Video by ID
    #3.9 Editing a Video
    #3.10 Deleting a Video

--- models/Video.js
+++ models/Video.js
@@ -12,7 +12,7 @@ const VideoSchema = new mongoose.Schema({
     description: String,
     views: {
         type: Number,
-        defulat: 0
+        default: 0
     },
     createdAt:{
         type: Date,
