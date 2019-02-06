5d0e9925ba7d426e4f3a695ea7bc5fdb63e9f33a
SeongJun Ha <agopwns@naver.com>
2019-02-06 15:51:47 +0900

    Youtube CC #18
    recap and Github userDetail page

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,36 +43,38 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (accessToken, refreshToken, profile, cb) => {
     const { 
-        _json: { id, avatar_url, name, email } 
+        _json: { id, avatar_url: avatarUrl, name, email } 
     } = profile;
     try {
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email });
         if(user){
-            user.gitHubId = id;
+            user.githubId = id;
             user.save();
             return cb(null, user);
         } 
         const newUser = await User.create({
             email,
             name,
-            gitHubId: id,
-            avatar_url: avatar_url
+            githubId: id,
+            avatarUrl
         });
         return cb(null, newUser);
-        
     } catch (error) {
-        return cb(error); 
+        return cb(error);
     }
-}
+};
 
 export const postGithubLogIn = (req, res) => {
     res.redirect(routes.home);
 }
 
 export const logout = (req, res) => {
-    // ToDo: Porcess Log Out
     req.logout();
     res.redirect(routes.home);
+};
+
+export const getMe = (req, res) => {
+    res.render("userDetail", { pageTitle : "User Detail", user: req.user });
 };
 
 export const userDetail = (req, res) => 
