19d6545977956debae39544523e18833f97500dc
SeongJun Ha <agopwns@naver.com>
2019-02-20 21:53:13 +0900

    YCC # 9.1
    Recording Video part One

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,21 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn")
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = (event) => {
+    console.log(event);
+};
+
+const startRecorindg = () => {
+    console.log(streamObject);
+    const videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.ondataavailabel = handleVideoData;
+    videoRecorder.start();
+    console.log(videoRecorder);
+};
+
+const getVideo = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
@@ -11,14 +25,19 @@ const startRecording = async () => {
       videoPreview.srcObject = stream;
       videoPreview.muted = true;
       videoPreview.play();
+      recordBtn.innerHTML = "Stop recording";
+      streamObject = stream;
+      startRecorindg();
     } catch (error) {
         recordBtn.innerHTML = "😂 Cant  record";
+    } finally {
         recordBtn.removeEventListener("click", startRecording);
     }
 };
 
 function init(){
     recordBtn.addEventListener("click", startRecording);
+    recordBtn.onclick = getVideo;
 }
 
 if (recorderContainer) {
